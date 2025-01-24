7e73f9f9dc06abd4853c59acae4016e5cfc1963c
avikagupta03 <123083752+avikagupta03@users.noreply.github.com>
2025-01-24 18:00:44 +0530

    Add files via upload

--- TaskTastic/lib/model/todo.dart
+++ TaskTastic/lib/model/todo.dart
@@ -2,21 +2,25 @@ class ToDo {
   String? id;
   String? todoText;
   bool isDone;
+  DateTime ?dueDate;
+  int ?priority;
 
   ToDo({
     required this.id,
     required this.todoText,
     this.isDone = false,
+    this.dueDate,
+    this.priority=0,
   });
 
   static List<ToDo> todoList() {
     return [
-      ToDo(id: '01', todoText: 'Morning Excercise', isDone: true ),
-      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true ),
-      ToDo(id: '03', todoText: 'Check Emails', ),
-      ToDo(id: '04', todoText: 'Team Meeting', ),
-      ToDo(id: '05', todoText: 'Work on mobile apps for 2 hour', ),
-      ToDo(id: '06', todoText: 'Dinner with Jenny', ),
+      ToDo(id: '01', todoText: 'Morning Excercise', isDone: true ,dueDate: DateTime.now().subtract(Duration(days: 1)),priority: 2),
+      ToDo(id: '02', todoText: 'Buy Groceries', isDone: true, dueDate: DateTime.now(),priority: 1),
+      ToDo(id: '03', todoText: 'Check Emails', dueDate: DateTime.now().add(Duration(days: 1)),priority: 3),
+      ToDo(id: '04', todoText: 'Team Meeting',dueDate: DateTime.now().add(Duration(days: 3)) ,priority: 2),
+      ToDo(id: '05', todoText: 'Work on mobile apps for 2 hour', dueDate: DateTime.now().add(Duration(days: 7)),priority: 1),
+      ToDo(id: '06', todoText: 'Dinner with Jenny', dueDate: DateTime.now().add(Duration(days: 10)),priority: 3),
     ];
   }
 }
